f2aa0f0c5521a8e2ef704d640eb9915a18288370
Henrik Korsgaard <korsgaard@cs.au.dk>
2023-09-22 10:33:45 +0200

    made a simple react examples with aktor

--- ftoda-react/src/App.js
+++ ftoda-react/src/App.js
@@ -1,36 +1,18 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Aktor from "./components/Aktor"
-
-const Q = gql`
-{
-  aktor(type:"Person"){id,type, navn, biografi}
-}
-`;
+import {AktorList, AktorPage} from "./components/Aktor"
 
 
 function App() {
-
-  const { data, loading, error } = useQuery(Q);
-  
-  if(error) {
-    return <pre>{error.message}</pre>
-  }
-
-  if (loading) return "Loading...";
-
   return (
-    
-    <div>
-      <h1>Medlemmer af Folketinget</h1>
-      <ul>
-        {data.aktor.map((a) => (
-          <Aktor key={a.id} name={a.navn} bio={a.biografi} />
-        ))}
-      </ul>
-    </div>
-  );
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AktorList />} />
+          <Route path="/aktor/*" element={<AktorPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
 }
 
 export default App;
--- ftoda-react/src/components/Aktor.js
+++ ftoda-react/src/components/Aktor.js
@@ -1,10 +1,80 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-function Aktor(props){
-    console.log(props)
+const Q = gql`
+{
+  aktor(type:"Person"){id, navn, titel, parti}
+}
+`;
+
+// next todo:
+// make the aktorlist
+// 0. Make a stats pages
+// 1. A tabel with columns
+// 2. Searchable (similar to grammar)
+
+
+function AktorList(){
+    const { data, loading, error } = useQuery(Q);
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
     return (
-        <li className="aktor">{props.name}: {props.bio}</li>
+      <div>
+        <h1>Medlemmer af Folketinget</h1>
+        <ul>
+          {data.aktor.map((a) => (
+            <AktorLink key={a.id} id={a.id} navn={a.navn} titel={a.titel} parti={a.parti} />
+          ))}
+        </ul>
+      </div>
     );
 }
 
-export default Aktor;
+function AktorLink(props){
+    return (
+        <li>
+        <a className="aktor" href={"aktor/" + props.navn + "-" + props.id}>{props.navn}</a>: {props.titel} - {props.parti} 
+        </li>
+    );
+}
+
+function AktorPage(){
+  
+    let uriParts = window.location.href.split("-")
+    let id = uriParts[uriParts.length-1]
+
+    const Q = gql`
+    {
+      aktor(id:${id}){navn, titel, parti, billede, uddannelse, personligt}
+    }`;
+
+    const { data, loading, error } = useQuery(Q);
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+    let aktor = data.aktor[0]
+    
+    return (
+        <div>
+        <h2><a href="/">Tilbage til oversigten</a></h2>
+            <div>
+                <h1>{aktor.navn}</h1>
+                <h4>{aktor.titel}</h4>
+                <img width="20%" src={aktor.billede} />
+                <p>{aktor.personligt}</p>
+                <h4>Uddannelse</h4>
+                <ul>
+                    {aktor.uddannelse.map((u)=>{
+                        return <li key={u}>{u}</li>
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export {
+    AktorLink,
+    AktorPage,
+    AktorList,
+}
